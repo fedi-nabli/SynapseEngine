2da368148b0351b49472a36beb6b1f34be997f70
fedi-nabli <fedinabli@gmail.com>
2025-05-11 14:00:52 +0200

    Update ICSV: Enhance ICSV class with additional methods for row and column access

--- ai_engine/includes/data/csv.hpp
+++ ai_engine/includes/data/csv.hpp
@@ -5,7 +5,7 @@
  * 
  * Author: Fedi Nabli
  * Date: 10 May 2025
- * Last Updated: 10 May 2025
+ * Last Updated: 11 May 2025
  */
 
 #pragma once
@@ -44,6 +44,51 @@ namespace SynapseParser
       void Parse();
       bool Validate();
 
+      inline std::size_t GetNumCols() const { return m_NumCols; };
+      inline std::size_t GetNumRows() const { return m_NumRows; };
+      inline const std::vector<Value>& GetRowByIndex(size_t idx) const
+      {
+        if (idx >= m_Data.size())
+        {
+          throw std::out_of_range("Row index out of bounds");
+        }
+
+        return m_Data[idx];
+      };
+
+      inline const std::vector<Value> GetColumnByIndex(size_t idx) const
+      {
+        if (idx >= m_NumCols)
+        {
+          throw std::out_of_range("Column index out of bounds");
+        }
+
+        std::vector<Value> column;
+        column.reserve(m_NumRows);
+
+        for (const auto& row : m_Data)
+        {
+          if (idx < row.size())
+          {
+            column.push_back(row[idx]);
+          }
+        }
+
+        return column;
+      }
+
+      inline const std::vector<Value> GetColumnByName(const std::string& name) const
+      {
+        auto it = std::find(m_Header.begin(), m_Header.end(), name);
+        if (it == m_Header.end())
+        {
+          throw std::out_of_range("Column name not found");
+        }
+
+        size_t idx = std::distance(m_Header.begin(), it);
+        return GetColumnByIndex(idx);
+      }
+
     private:
       void PrintStats();
       void CopyData(CSV* csv);
@@ -57,7 +102,7 @@ namespace SynapseParser
 
       std::string m_Filename;
       std::string m_FilePath;
-      uint m_NumCols;
-      uint m_NumRows;
+      std::size_t m_NumCols;
+      std::size_t m_NumRows;
   };
 };
