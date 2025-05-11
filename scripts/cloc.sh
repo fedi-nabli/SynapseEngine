c268bdf85df453d8794fe1597365a16ef9a7ff9e
fedi-nabli <fedinabli@gmail.com>
2025-05-11 15:11:10 +0200

    Remove cloc.sh from cloc script: exclude script file from code line count

--- scripts/cloc.sh
+++ scripts/cloc.sh
@@ -3,4 +3,4 @@
 cloc ai_engine cmake csv_parser/src csv_parser/includes csv_parser/CMakeLists.txt csv_parser/build.zig \
   json_parser/src json_parser/includes json_parser/CMakeLists.txt json_parser/build.zig scripts \
   synapse_math/src synapse_math/include synj_parser/src synj_parser/includes synj_parser/CMakeLists.txt synj_parser/build.zig \
-  build.sh cloc.sh CMakeLists.txt
+  build.sh CMakeLists.txt
