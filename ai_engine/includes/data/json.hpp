e9e59b7041dc3a1f35ffcd30588c0e9ea7a9adc8
fedi-nabli <fedinabli@gmail.com>
2025-05-13 10:18:44 +0200

    Enhance IJson class: add getter methods for model attributes and implement bounds checking for weight retrieval

--- ai_engine/includes/data/json.hpp
+++ ai_engine/includes/data/json.hpp
@@ -18,6 +18,25 @@ namespace SynapseParser
 
       void Parse();
 
+      inline std::string GetSchemaVersion() const { return m_SchemaVersion; };
+      inline std::string GetRunID() const { return m_RunID; };
+      inline std::string GetModelName() const { return m_ModelName; };
+      inline std::string GetModelType() const { return m_ModelType; };
+      inline std::string GetTarget() const { return m_Target; };
+      inline uint32_t GetEpochsTrained() const { return m_EpochsTrained; };
+      inline double GetFinalLoss() const { return m_FinalLoss; };
+      inline double GetBias() const { return m_Bias; };
+      inline std::vector<double> GetWeights() const { return m_Weights; };
+      inline double GetWeightByIndex(std::size_t idx)
+      {
+        if (idx >= m_Weights.size() || idx < 0)
+        {
+          throw std::out_of_range("Item Index out of range");
+        }
+
+        return m_Weights[idx];
+      };
+
     private:
       void CopyData(Json* json);
 
