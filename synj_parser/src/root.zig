166df476efee5753e993980319107a8fe5a80a57
fedi-nabli <fedinabli@gmail.com>
2025-05-14 17:00:05 +0200

    Add SYNJ structure definitions for model configuration and parsing + setup entry functions

--- synj_parser/src/root.zig
+++ synj_parser/src/root.zig
@@ -1,10 +1,53 @@
-const std = @import("std");
-const testing = std.testing;
+// root.zig - Root file for SYNJ Parser
+//
+// This file contains the main functions for parsing and freeing SYNJ data,
+// as well as managing the global allocator.
+//
+// Author: Fedi Nabli
+// Date: 14 May 2025
+// Last Modified: 14 May 2025
 
-pub export fn add_synj(a: i32, b: i32) i32 {
-    return a + b;
+const std = @import("std");
+
+const Synj = @import("synj.zig").Synj;
+
+// Global static allocator
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+// Reference to the allocator interface
+const global_allocator = gpa.allocator();
+
+pub export fn synj_parser_parse(
+    buffer_ptr: [*]const u8,
+    buffer_len: usize,
+) ?*Synj {
+    const buf = buffer_ptr[0..buffer_len];
+    const synj_ptr = global_allocator.create(Synj) catch return null;
+
+    _ = buf;
+
+    return synj_ptr;
 }
 
-test "basic add functionality" {
-    try testing.expect(add_synj(3, 7) == 10);
+pub export fn synj_parser_free(synj: *Synj) void {
+    if (synj.model_name != null) {
+        global_allocator.free(std.mem.sliceTo(synj.model_name.?, 0));
+    }
+
+    if (synj.csv_path != null) {
+        global_allocator.free(std.mem.sliceTo(synj.csv_path.?, 0));
+    }
+
+    if (synj.target != null) {
+        global_allocator.free(std.mem.sliceTo(synj.target.?, 0));
+    }
+
+    // TODO: free train test split array
+    // TODO: free string array memory (features & classes)
+    // TODO: free early stop
+
+    if (synj.output_path != null) {
+        global_allocator.free(std.mem.sliceTo(synj.output_path.?, 0));
+    }
+
+    global_allocator.destroy(synj);
 }
---
+++ synj_parser/src/synj.zig
@@ -0,0 +1,32 @@
+// synj.zig - SYNJ Structure definitions
+//
+// This file defines structures and enums related to
+// the SYNJ DSL structure to be parsed for the model configuration
+//
+// Author: Fedi Nabli
+// Date: 14 May 2025
+// Last Modified: 14 May 2025
+
+pub const ModelType = enum {
+    LinearRegression,
+    LogisticRegression,
+};
+
+pub const EarlyStop = struct {
+    patience: u32,
+};
+
+pub const Synj = struct {
+    model_name: ?[*:0]u8,
+    model_type: ModelType,
+    csv_path: ?[*:0]u8,
+    target: ?[*:0]u8,
+    train_test_split: [2]u8,
+    features: ?[*][*:0]u8,
+    classes: ?[*][*:0]u8,
+    epochs: u32,
+    learning_rate: f64,
+    batch_size: ?u32,
+    early_stop: ?EarlyStop,
+    output_path: ?[*:0]u8,
+};
