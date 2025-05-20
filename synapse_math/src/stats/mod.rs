6f1dd0f1fd897162531ed7a663057ecf164f4bf2
fedi-nabli <fedinabli@gmail.com>
2025-05-20 10:56:56 +0200

    Update documentation in stats/mod.rs to include author and date information

--- synapse_math/src/stats/mod.rs
+++ synapse_math/src/stats/mod.rs
@@ -1,3 +1,11 @@
+/// stats/mod.rs - Math Engine Stats module
+/// 
+/// This module exposes core statistics functions
+/// 
+/// Author: Fedi Nabli
+/// Date: 20 May 2025
+/// Last Modified: 20 May 2025
+
 pub mod stats;
 
 pub use stats::{mean, variance, std_dev, normalize, covariance, correlation};
